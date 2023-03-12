92b387fc8e27a65c17ce95256a45a2f80c0c1dcb
JuanJoseSolo <juanjose.solorzano.c@gmail.com>
2023-03-12 07:52:01 -0600

    Added help comments in cp-items.ps1

--- cp-items.ps1
+++ cp-items.ps1
@@ -1,9 +1,15 @@
 param($item, $source, $destination)
 
+if($null -eq $source -or $null -eq $destination -or $null -eq $item){
+    echo "You need the following parameters:
+          [*] item: the item to be copied.
+          [*] source: the root path where is located the item.
+          [*] destination: the location where the item will be pasted."
+}
 if($null -ne $source -and $null -ne $destination)
 {
     Get-ChildItem -Path $source -Filter $item -File -Recurse | ForEach-Object {
     $newName = '{0}_{1}_{2}' -f $_.Directory.Parent.Name, $_.Directory.Name, $_.Name
     $_ | Copy-Item -Destination (Join-Path -Path $destination -ChildPath $newName)
-}
+    }
 }
